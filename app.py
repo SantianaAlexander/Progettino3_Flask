e0ef297e8ab104d8c792a110a0fd995a6014ddee
Alexander <santiana.a@iisgalvanimi.edu.it>
2024-11-28 20:48:23 +0100

    FINE PROGETTINO 3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from models import db, ListaSpesa
 
@@ -16,8 +16,32 @@ with app.app_context():
 #rotta principale
 @app.route('/')
 def home():
-    return render_template('index.html')
+    lista_spesa = ListaSpesa.query.all()
+    return render_template('index.html', lista_spesa=lista_spesa)
     
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi_prodotto():
+    elemento = request.form['elemento']
+    prodotto = ListaSpesa(elemento=elemento)
+    if prodotto:
+        db.session.add(prodotto)
+        db.session.commit()
+    return (redirect(url_for('home')))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi_oggetto(indice):
+    elemento_da_rimuovere = ListaSpesa.query.get(indice)
+    if elemento_da_rimuovere:
+        db.session.delete(elemento_da_rimuovere)
+        db.session.commit()
+    return(redirect(url_for('home')))
+
+@app.route('/svuota', methods=['POST'])
+def svuota_lista():
+    ListaSpesa.query.delete()
+    db.session.commit() 
+    return redirect(url_for('home'))
+
 
 #avvio dell'app Flask - Sempre mettere alla fine
 if __name__ == '__main__':
